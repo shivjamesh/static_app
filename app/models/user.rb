62d3bc7420a1b133cf3a7c90935520e0b940d4b6
shiv <shiva.shivjamesh@gmail.com>
2017-03-21 19:58:09 +0530

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,8 +52,9 @@ class User < ApplicationRecord
     end
 
     def create_reset_digest
-      self.reset_token = User.new.token
-      update_columns(reset_digest:  FILL_IN, reset_sent_at: FILL_IN)
+      self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
   end
 
     def password_reset_expired?
